c67011318fa75558e4e6835fcd894845499b6f19
AIDepzaiphaibit <95866897+AIDepzaiphaibit@users.noreply.github.com>
2023-06-24 16:10:50 +0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+# Comment
+from import as
+while
+for in
+if else or and
+with in as
+<+-*/=>!=
+True False None
+class ():
+	def __init__():
+		super()
+		return
+is not
+raise
+list map
+int str float bool type
+1234567890
